fea2898b1f311ded6fde97b630b248ae1e7a4881
Dhavalkumar vadgama <dhaval.it@gmail.com>
2022-02-02 10:33:06 +0000

    updated: finalize script to hide grep error moved: variable replacement codes from finalize script to env.sh fixed: sed errors updated: env.sh to replace vars

--- deps/env.sh
+++ deps/env.sh
@@ -9,8 +9,24 @@ setup_app(){
     export EXTERNAL_DOMAIN=${EXTERNAL_DOMAIN:-$(jq -r '.uris[0]' <<<"${VCAP_APPLICATION}")}
 }
 
+replace_vars(){
+
+    env | while IFS='=' read -r key val; do
+            if grep -q "\\\${$key}" "${APP_ROOT}/alertmanager.yml" > /dev/null 2>&1
+            then
+                if echo ${val} | grep -q http
+                then 
+                    val=$(perl -ne 'print quotemeta($_)' <<< ${val})
+                    val=${val::-1}
+                fi
+                sed -i 's@${'"${key}"'}@'"${val}"'@g' "${APP_ROOT}/alertmanager.yml"
+            fi
+        done
+}
+
 main(){
     setup_app
+    replace_vars
 }
 
 main
